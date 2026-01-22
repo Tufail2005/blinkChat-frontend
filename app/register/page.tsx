f458256239ddce4bfe2c8f319c5ecba4fbeabd50
ashisdutta <ashisdutta795@gmail.com>
2026-01-22 13:18:51 +0530

    first commit

---
+++ app/register/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import {InputButtonGroup} from "@/components/ui/input-button-group"
+export default function Home() {
+    return (
+        <InputButtonGroup/>
+    );
+}
